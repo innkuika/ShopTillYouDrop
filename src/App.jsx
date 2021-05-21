4118d286efa7a8e813180ac2f475238d929bdd76
Jessica Wu <>
2021-05-21 21:37:35 +0000

    navigation example

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,62 @@
-
 import React, { useRef, useEffect } from 'react';
 import MyD3Component from "./MyD3Component.jsx";
 import './App.css';
-
-
-
+import { NavLink, Switch, Route } from 'react-router-dom';
 
 
 /* App */
 function App() {
     return (
-        <div>
+        <div className="app">
+        // <Navigation />
+        // <Main />
           <p>A bar chart! </p>
         
           <div  className="barChart">
             <MyD3Component data={[1,5,6,3]}/>
-        </div>
+          </div>
         </div>
     )
 }
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li>
+      <li><NavLink exact activeClassName="current" to='/contact'>Contact</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Home}></Route>
+    <Route exact path='/about' component={About}></Route>
+    <Route exact path='/contact' component={Contact}></Route>
+  </Switch>
+);
+
+const Home = () => (
+  <div className='home'>
+    <h1>Welcome to my portfolio website</h1>
+    <p> Feel free to browse around and learn more about me.</p>
+  </div>
+);
+
+const About = () => (
+  <div className='about'>
+    <h1>About Me</h1>
+    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+  </div>
+);
+
+const Contact = () => (
+  <div className='contact'>
+    <h1>Contact Me</h1>
+    <p>You can reach me via email: <strong>hello@example.com</strong></p>
+  </div>
+);
 
 export default App;
