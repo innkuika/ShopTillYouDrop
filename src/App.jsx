2bd5fc7e9e268be2deefd7c5bc477316d535af7f
innkuika <innku.ika@gmail.com>
2021-05-22 00:45:04 -0700

    home page & dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,79 @@
-import React, { useRef, useEffect } from 'react';
+import React, {useRef, useEffect} from 'react';
 import MyD3Component from "./MyD3Component.jsx";
+import './reset.css';
 import './App.css';
-import { NavLink, Switch, Route } from 'react-router-dom';
+import './dropdownsearch.css'
+import {NavLink, Switch, Route} from 'react-router-dom';
+import SelectSearch from 'react-select-search';
+import fuzzySearch from "./fuzzySearch";
 
 
 /* App */
 function App() {
     return (
-        <div className="app">
-        // <Navigation />
-        // <Main />
-          <p>A bar chart! </p>
-        
-          <div  className="barChart">
-            <MyD3Component data={[1,5,6,3]}/>
-          </div>
+        <div>
+            <Switch>
+                <Route exact path='/' component={Home}></Route>
+                <Route exact path='/about' component={About}></Route>
+                <Route exact path='/contact' component={Contact}></Route>
+            </Switch>
         </div>
     )
 }
 
 const Navigation = () => (
-  <nav>
-    <ul>
-      <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
-      <li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li>
-      <li><NavLink exact activeClassName="current" to='/contact'>Contact</NavLink></li>
-    </ul>
-  </nav>
+    <nav>
+        <ul>
+            <li><NavLink exact activeClassName="current" to='/'>Home</NavLink></li>
+            <li><NavLink exact activeClassName="current" to='/about'>About</NavLink></li>
+            <li><NavLink exact activeClassName="current" to='/contact'>Contact</NavLink></li>
+        </ul>
+    </nav>
 );
 
-const Main = () => (
-  <Switch>
-    <Route exact path='/' component={Home}></Route>
-    <Route exact path='/about' component={About}></Route>
-    <Route exact path='/contact' component={Contact}></Route>
-  </Switch>
-);
+
+
+
+const options = [
+    {name: 'Swedish', value: 'sv'},
+    {name: 'English', value: 'en'},
+
+];
+
 
 const Home = () => (
-  <div className='home'>
-    <h1>Welcome to my portfolio website</h1>
-    <p> Feel free to browse around and learn more about me.</p>
-  </div>
+    <div className='home page'>
+        <blue-title>SHOP 'TIL YOU DROP</blue-title>
+        <div className='text-content-wrap'>
+            <p> Tuition is only the sticker price - you might be eligible for a big discount! Estimate the <span
+                className='italic-bold'>real</span> cost of college, for schools across the country.</p>
+        </div>
+        <div className='center-img'>
+            <img src="/src/graphics/shoppingcart.png"/>
+        </div>
+        <div className='bold-text home'>
+            Start typing to pick a school
+        </div>
+        <SelectSearch options={options} value="" name="language" search={true} filterOptions={fuzzySearch}/>
+    </div>
 );
 
 const About = () => (
-  <div className='about'>
-    <h1>About Me</h1>
-    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-    <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
-  </div>
+    <div className='about'>
+        <h1>About Me</h1>
+        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et
+            quos harum excepturi dolorum molestias?</p>
+        <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et
+            quos harum excepturi dolorum molestias?</p>
+    </div>
 );
 
 const Contact = () => (
-  <div className='contact'>
-    <h1>Contact Me</h1>
-    <p>You can reach me via email: <strong>hello@example.com</strong></p>
-  </div>
+    <div className='contact'>
+        <Navigation/>
+        <h1>Contact Me</h1>
+        <p>You can reach me via email: <strong>hello@example.com</strong></p>
+    </div>
 );
 
 export default App;
